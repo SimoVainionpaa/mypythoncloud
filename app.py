2cd099d912544dac4dfd8c757d5839e9272fb7dd
Simo Vainionpää <smvnp1@gmail.com>
2023-03-16 09:36:00 +0200

    updated

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, Response, jsonify
 
 app = Flask(__name__)
 
@@ -6,5 +6,15 @@ app = Flask(__name__)
 def root():
     return "<h1>hello</h1>"
 
+
+@app.route("/customers")
+def get_customers():
+    python_list = [{"id": 1, "name": "jack"}, {"id": 2, "name": "hannah"}]
+    my_response = jsonify(python_list)
+    return my_response
+    
+
 if __name__ == "__main__":
     app.run()
+
+
